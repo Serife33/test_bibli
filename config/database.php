433a7aa8455699d30b1282fffe2ea119bace4a80
AIRASSART64 <irassarta@gmail.com>
2025-08-25 15:30:15 +0200

    indexBooksFirstVersion

--- config/database.php
+++ config/database.php
@@ -1,3 +1,43 @@
 <?php
-echo "Hello, World!"
+    // logique de connexion a la database
+    // Information pour se connecter
+    // l'endroit où est ma database
+    // function qui crée et renvoi une connection à la db
+function dbConnexion() {
+    $host = "localhost";
+        //  Le nom de la DB
+    $dbname = "Bibliotheque";
+        //  Identifiant de connexion
+    $username = "root";
+        //  password laptop : "*KLp4osj&3PC!29^$^tq%64Q7z7$2i$^"
+    $password = "";
+        // port
+    $port= 3306;
+        // charset
+    $charset= "utf8mb4";
+
+
+    //transforme mes variable en global (accessible partout)
+    // global $host, $dbname, $password, $username, $port, $charset;
+
+    try {
+        // mes param de co
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port";
+        // fait mon objet de co
+        $pdo = new PDO($dsn, $username, $password);
+        // comment récupérer les exception (erreurs)
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //comment me renvoyer les données
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $pdo;
+
+    } catch (PDOException $e) {
+        die("Erreur durant la co la db: " . $e->getMessage());
+    }
+
+}
+
+// dbConnexion();
+// var_dump($pdo);
 ?>
---
+++ public/index.php
@@ -0,0 +1,58 @@
+<?php
+    require_once '../config/database.php'; // connexion à la db dans laquelle se trouve la table tasks
+     $pdo = dbConnexion();
+
+    // dans la db on accéde à toutes les données de la table tasks d
+     $sql = "SELECT * FROM livres ";
+     $requestDb = $pdo->prepare($sql);   
+     $requestDb->execute();
+     $livres = $requestDb->fetchAll(PDO::FETCH_ASSOC);
+
+     
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Test_bibli</title>
+    <link rel="stylesheet" href="../assets/styles.css">
+   
+</head>
+<body>
+    <?php
+    //   include '../includes/header.php';
+    ?>
+    <div class="Container">
+          <h2> Tous les livres </h2>
+            
+     <ul class="books-list">
+         <!-- boucle pour afficher chaque tâches contenues dans la table livres -->
+        <?php foreach ($livres as $livre) : ?> 
+         
+     
+            <li>
+                 <!-- intégration des chacune des valeurs contenues dans la table tasks pour une tâche donnée -->
+                    <h3><?= htmlspecialchars($livre['Titre']) ?></h3>
+                     <div>
+                    <label class="" > <?= htmlspecialchars($livre['Auteur']) ?></label>
+                    <label class=""> Année : <?= ($livre['Annee']) ?></label>
+                    <label> Disponible : <?= ($livre['Disponibilite'] == 1 ? "Oui" : "Non") ?></label>
+                    </div>
+                    <div class = "">
+                    <a class="" href="voir.php?id=<?= ($livre['id']) ?>">Voir</a>
+                    <a class="" href="../config/edit.php?id=<?= ($livre['id']) ?>">Modifier</a>
+                    <a class="" href="../config/delete.php?id=<?= ($livre['id']) ?>">Supprimer</a>
+                    </div>
+
+            </li>
+         <?php endforeach; ?>
+     </ul>
+ </div>
+
+  <?php
+      include '../includes/footer.php';
+    ?>
+</body>
+</html>
